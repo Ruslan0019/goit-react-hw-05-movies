b186b14625d9496e2fa7510ab2e4f25f9f1c1560
Ruslan0019 <133685041+Ruslan0019@users.noreply.github.com>
2024-01-08 00:06:21 +0200

    1
    1

---
+++ src/components/Api/Api.jsx
@@ -0,0 +1,40 @@
+// import React from 'react'
+
+// const options = {
+//   method: 'GET',
+//   headers: {
+//     accept: 'application/json',
+//     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiMmM4ZmNlODBkNWY2YWRhNDI2MmIyY2Q0OTQwNTY1MiIsInN1YiI6IjY1NDIzNzExMWFjMjkyMDBlMTE4NGQyNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.z31pb3WMAG3QsgPH8x9AM27sCQZfGvUnbyhPmGdRJT0'
+//   }
+// };
+
+// fetch('https://api.themoviedb.org/3/trending/all/day?language=en-US', options)
+//   .then(response => response.json())
+//   .then(response => console.log(response))
+//   .catch(err => console.error(err));
+
+//   fetch('https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1&query=${query}', options)
+//   .then(response => response.json())
+//   .then(response => console.log(response))
+//   .catch(err => console.error(err));
+
+//   fetch('https://api.themoviedb.org/3/movie/movie_id?language=en-US', options)
+//   .then(response => response.json())
+//   .then(response => console.log(response))
+//   .catch(err => console.error(err));
+
+//   fetch('https://api.themoviedb.org/3/movie/movie_id/credits?language=en-US', options)
+//   .then(response => response.json())
+//   .then(response => console.log(response))
+//   .catch(err => console.error(err));
+
+//   fetch('https://api.themoviedb.org/3/movie/movie_id/reviews?language=en-US&page=1', options)
+//   .then(response => response.json())
+//   .then(response => console.log(response))
+//   .catch(err => console.error(err));
+// export default function Api() {
+//   return (
+//     <div>Api</div>
+//   )
+// }
+
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
-export const App = () => {
+import React from 'react';
+import Home from './Home/Home';
+
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiMmM4ZmNlODBkNWY2YWRhNDI2MmIyY2Q0OTQwNTY1MiIsInN1YiI6IjY1NDIzNzExMWFjMjkyMDBlMTE4NGQyNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.z31pb3WMAG3QsgPH8x9AM27sCQZfGvUnbyhPmGdRJT0` 
+  }
+};
+
+const searchKeyword = 'dog'; 
+
+fetch(`https://api.themoviedb.org/3/search/movie?query=${searchKeyword}&include_adult=false&language=en-US&page=1`, options)
+  .then(response => response.json())
+  .then(response => console.log(response))
+  .catch(err => console.error(err));
+const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <div></div>
+      {/* <Api/> */}
+      <Home />
+    </>
   );
 };
+export default App;
